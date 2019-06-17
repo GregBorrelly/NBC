fefa2ff99a39d962490e1997d9a53926e10e2038
Greg Borrelly <gregborrelly@gmail.com>
2019-06-16 22:43:47 -0400

    Bugfix: Removing redundant array left from prototyping. Running files through beautify

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const emails = ['greg', 'dee', 'nicole']
-
 const fetchFromLocalStorage = (item) => {
     return JSON.parse(localStorage.getItem(item))
 }
 
-
-const populateNewMembers = (arr=[]) => {
+const populateNewMembers = (arr = []) => {
     const ul = document.getElementById("members")
     arr.forEach(member => {
-        let li = document.createElement('li') 
+        let li = document.createElement('li')
         li.appendChild(document.createTextNode(member))
         ul.appendChild(li)
     })
@@ -16,7 +13,7 @@ const populateNewMembers = (arr=[]) => {
 
 const saveMember = (member) => {
     const emails = fetchFromLocalStorage('nbc-newsletter-emails') || []
-    if(!emails.includes(`Welcome ${member} !`)){
+    if (!emails.includes(`Welcome ${member} !`)) {
         emails.unshift(`Welcome ${member} !`)
         localStorage.setItem('nbc-newsletter-emails', JSON.stringify(emails))
         populateNewMembers([`Welcome ${member} !`])
@@ -26,15 +23,14 @@ const saveMember = (member) => {
 
 window.onload = () => {
     document.getElementById('nbc-email-form').addEventListener('submit', () => {
-        
+
     })
     const emails = fetchFromLocalStorage('nbc-newsletter-emails') || []
     const throttledInterval = fetchFromLocalStorage('nbc-newsletter-throttledInterval')
-    
 
-    if(!throttledInterval){
+
+    if (!throttledInterval) {
         //Handle 
     }
     populateNewMembers(emails)
 }
-
